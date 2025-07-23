a13df2e002b58a8234ba903c380df96f46409491
Sabare1 <sabaregirivasan@gmail.com>
2025-07-23 12:40:38 +0530

    challenge #4

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import reactLogo from './assets/react.svg'
+import Header from './Header'
 
 createRoot(document.getElementById('root')).render(
   <ReasonForReact></ReasonForReact>
@@ -16,27 +16,17 @@ function ReasonForReact(){
   )
 }
 
-function Header(){
-  return(
-    <>
-      <header>
-        <img src={reactLogo} width="40px"alt="react logo"></img>
-      </header>
-    </>
-  )
-}
-
 function MainContent(){
   return(
-    <>
+    <main>
       <h1>Reasons for learning react</h1>
-        <ol>
-            <li>React is a popular library, so I will be able to
-            fit in with all the coolest devs out there! 😎</li>
-            <li>I am more likely to get a job as a front end developer
-            if I know React</li>
-        </ol>
-    </>
+      <ol>
+          <li>React is a popular library, so I will be able to
+          fit in with all the coolest devs out there! 😎</li>
+          <li>I am more likely to get a job as a front end developer
+          if I know React</li>
+      </ol>
+    </main>
   )
 }
 
