d155ecd70a7a685333f97f18695445a97c55e251
Sabare1 <sabaregirivasan@gmail.com>
2025-07-23 11:52:29 +0530

    challenge #3

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -10,14 +10,8 @@ function ReasonForReact(){
   return(
     <>
       <Header></Header>
-      <h1>Reasons for learning react</h1>
-      <ol>
-          <li>React is a popular library, so I will be able to
-          fit in with all the coolest devs out there! 😎</li>
-          <li>I am more likely to get a job as a front end developer
-          if I know React</li>
-      </ol>
-      <footer>© 2025 Vasan development. All rights reserved.</footer>
+      <MainContent></MainContent>
+      <Footer></Footer>
     </>
   )
 }
@@ -29,5 +23,25 @@ function Header(){
         <img src={reactLogo} width="40px"alt="react logo"></img>
       </header>
     </>
+  )
+}
+
+function MainContent(){
+  return(
+    <>
+      <h1>Reasons for learning react</h1>
+        <ol>
+            <li>React is a popular library, so I will be able to
+            fit in with all the coolest devs out there! 😎</li>
+            <li>I am more likely to get a job as a front end developer
+            if I know React</li>
+        </ol>
+    </>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>© 2025 Vasan development. All rights reserved.</footer>
   )
 }
