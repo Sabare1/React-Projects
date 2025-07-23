411a303bae7a64648d3d80148c48cd2bebcb5039
Sabare1 <sabaregirivasan@gmail.com>
2025-07-23 11:37:50 +0530

    challenge #1

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import reactLogo from './assets/react.svg'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ReasonForReact></ReasonForReact>
 )
+
+function ReasonForReact(){
+  return(
+    <main>
+      <header>
+        <img src={reactLogo} width="40px"alt="react logo"></img>
+      </header>
+      <h1>Reasons for learning react</h1>
+      <ol>
+          <li>React is a popular library, so I will be able to
+          fit in with all the coolest devs out there! 😎</li>
+          <li>I am more likely to get a job as a front end developer
+          if I know React</li>
+      </ol>
+      <footer>© 2025 Vasan development. All rights reserved.</footer>
+    </main>
+  )
+}
