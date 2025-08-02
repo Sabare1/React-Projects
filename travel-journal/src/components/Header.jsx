7a18464839f11d834596ad214ff59b670c7d11a5
Sabare1 <sabaregirivasan@gmail.com>
2025-08-02 12:57:25 +0530

    nearly done with the entry component

--- travel-journal/src/components/Header.jsx
+++ travel-journal/src/components/Header.jsx
@@ -4,10 +4,8 @@ import '../index.css'
 export default function Header(){
     return(
         <header>
-            <main className="main-container">
-                <img src={globeLogo} alt="globe icon" id="globe-icon"></img>
-                <span id="title-txt">my travel journal</span>
-            </main>
+            <img src={globeLogo} alt="globe icon" id="globe-icon"></img>
+            <h1 id="title-txt">my travel journal</h1>
         </header>
     )
 }
