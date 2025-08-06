88e164e0d9fe4b54a3a0511f1634c188714beba5
Sabare1 <sabaregirivasan@gmail.com>
2025-08-06 13:00:55 +0530

    added some ingredients functionality

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,10 +1,24 @@
 export default function Main(){
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const newIng = ingredients.map((item) => {
+                    return <li>{item}</li>
+    });
+    function submitEvent(e){
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        ingredients.push(newIngredient);
+        console.log(ingredients);
+    }
     return(
         <main>
-            <form className="input-form">
-                <input placeholder="e.g. oregano" aria-label="Add Ingredient" className="user-input"></input>
+            <form className="input-form" onSubmit={submitEvent}>
+                <input placeholder="e.g. oregano" aria-label="Add Ingredient" className="user-input" name='ingredient'></input>
                 <button className="submit-btn">+ Add Ingredient</button>
             </form>
+            <ul>
+                {newIng}
+            </ul>
         </main>
     )
 }
