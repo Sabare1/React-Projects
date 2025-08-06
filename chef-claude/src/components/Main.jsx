f48594479e24ac755a09921a019d9c190b5097e0
Sabare1 <sabaregirivasan@gmail.com>
2025-08-06 11:16:20 +0530

    added the static parts of chef claude

---
+++ chef-claude/src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+    return(
+        <main>
+            <form className="input-form">
+                <input placeholder="e.g. oregano" aria-label="Add Ingredient" className="user-input"></input>
+                <button className="submit-btn">+ Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
