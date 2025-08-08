350cdf36d375b33e99453e8eda2efab8ffd8f8f2
Sabare1 <sabaregirivasan@gmail.com>
2025-08-08 12:29:31 +0530

    reverted back

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
-function signUp(formData){
-  const email = formData.get("email");
-  const password = formData.get("password");
-  console.log(password);
-}
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
   return (
-    <section>
-      <h1>Signup form</h1>
-      <form action={signUp}>
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
-        <label htmlFor='password'>Password:</label>
-        <input id="password" type="password" name="password"></input>
-        <br />
-
-        <button type='submit'>submit</button>
-      </form>
-    </section>
+    <div className='container'>
+      <Header></Header>
+      <Main></Main>
+    </div>
   )
 }
+
+export default App
