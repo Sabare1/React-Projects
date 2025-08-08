f933e438432598a747b292c91a7158e17031243c
Sabare1 <sabaregirivasan@gmail.com>
2025-08-08 12:28:45 +0530

    learning forms in react

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className='container'>
-      <Header></Header>
-      <Main></Main>
-    </div>
-  )
+function signUp(formData){
+  const email = formData.get("email");
+  const password = formData.get("password");
+  console.log(password);
 }
 
-export default App
+export default function App() {
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form action={signUp}>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+        
+        <label htmlFor='password'>Password:</label>
+        <input id="password" type="password" name="password"></input>
+        <br />
+
+        <button type='submit'>submit</button>
+      </form>
+    </section>
+  )
+}
