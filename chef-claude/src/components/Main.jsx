9a011b658563b5281ec8cb67c01b61a54944f009
Sabare1 <sabaregirivasan@gmail.com>
2025-08-09 12:59:58 +0530

    added recipe container and some styling

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -16,9 +16,20 @@ export default function Main(){
                 <input placeholder="e.g. oregano" aria-label="Add Ingredient" className="user-input" name='ingredient'></input>
                 <button className="submit-btn">+ Add Ingredient</button>
             </form>
-            <ul>
-                {newIng}
-            </ul>
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul>
+                    {newIng}
+                </ul>
+                <div className='recipe-container'>
+                    <div className='about-recipe-gen'>
+                        <p id='hype-user'>Ready for a recipe?</p>
+                        <p id='actual-ins'>Generate a recipe from your set of ingredients</p>
+                    </div>
+                    <button className='get-recipe-btn'>Get a recipe</button>
+                </div>
+            </section>
+            
         </main>
     )
 }
