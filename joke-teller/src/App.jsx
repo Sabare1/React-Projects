a4f21df95afa6941f707b9572a8cb8f4ebc9f492
Sabare1 <sabaregirivasan@gmail.com>
2025-08-09 11:17:24 +0530

    added conditional rendering to the joke teller

--- joke-teller/src/App.jsx
+++ joke-teller/src/App.jsx
@@ -4,6 +4,7 @@ import jokesData from './jokesData'
 function App() {
   const jokeJSX = jokesData.map(
     (joke)=> <Joke
+    key = {joke.id}
     setup = {joke.setup}
     punch = {joke.punchline}
     ></Joke>
--- joke-teller/src/components/Joke.jsx
+++ joke-teller/src/components/Joke.jsx
@@ -1,9 +1,20 @@
+import React from 'react'
+
 export default function Joke({setup, punch, upVotes, downVotes, comments, isPun}){
-    console.log(isPun);
+    const[isShown, setIsShown] = React.useState(false)
+    let punchState = isShown ? "hide" : "show";
+    if(!isShown){
+        punch = "";
+    }
+    function changeVisibility(){
+        setIsShown((prevIsShown) => !prevIsShown)
+    }
+
     return(
         <>
             {setup && <p>Setup: {setup}</p>}
-            <p>Punch: {punch}</p>
+            {punch && <p>Punch: {punch}</p>}
+            <button id='showPunch' onClick={changeVisibility}>{punchState} punch</button>
             <hr/>
         </>
     )
