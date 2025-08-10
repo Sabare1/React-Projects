7cc2e7c4fd905bff5e69f3ffd0c9a0044d8d6be4
Sabare1 <sabaregirivasan@gmail.com>
2025-08-10 12:41:53 +0530

    oopsie

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,49 +1,14 @@
 import React from "react"
-import Count from "./Count"
+import Header from "./Header"
+import Body from "./Body"
+
+const [userName, setUserName] = React.useState("Joe")
+
 export default function App() {
-    const [count, setCount] = React.useState(0)
-
-    function add() {
-        setCount(prevCount => prevCount + 1)
-    }
-
-    function subtract() {
-        setCount(prevCount => prevCount - 1)
-    }
-    
-    /**
-     * Challenge:
-     * - Create a new component called `Count`
-     *    - It should receive a prop called `number`, whose value
-     *      is the current value of our count
-     *    - Have the component render the h2.count element below
-     *      and display the incoming prop `number`
-     * - Replace the h2.count below with an instance of
-     *   the new Count component, passing the correct value
-     *   to its `number` prop.
-     * - After doing this, everything should be working the
-     *   same as before.
-     */
-
     return (
-        <main className="container">
-            <div className="counter">
-                <button
-                    className="minus"
-                    onClick={subtract}
-                    aria-label="Decrease count"
-                >-</button>
-
-                <Count
-                number={count}
-                ></Count>
-
-                <button
-                    className="plus"
-                    onClick={add}
-                    aria-label="Increase count"
-                >+</button>
-            </div>
+        <main>
+            <Header name={userName}/>
+            <Body name={userName}/>
         </main>
     )
 }
