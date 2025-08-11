96306e00e74d22c21d034ec26fde9bb5c551e93c
Sabare1 <sabaregirivasan@gmail.com>
2025-08-11 11:46:51 +0530

    sound pad styling with help of dynamic styling

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,13 +1,24 @@
+import padsData from "./pads"
 import React from "react"
-import Header from "./Header"
-import Body from "./Body"
+export default function App(props) {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from pads.js
+     * 2. Map over that state array and display each one
+     *    as a <button> (CSS is already written for you)
+     *    (Don't worry about using the "on" or "color" 
+     *    properties yet)
+     */
+    const btnClr = props.darkMode ? "#222222" : "#cccccc";
+    const[pads, setPads] = React.useState(padsData);
+    const btnArr = pads.map((pad) => <button style= {{backgroundColor : btnClr}}key={pad.id}></button>)
 
-export default function App() {
-    const [userName, setUserName] = React.useState("Joe")
     return (
         <main>
-            <Header name={userName}/>
-            <Body name={userName}/>
+            <div className="pad-container">
+                {btnArr}
+            </div>
         </main>
     )
 }
