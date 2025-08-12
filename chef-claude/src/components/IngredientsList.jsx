37b2f59fdd62df4363e84f1622d4887ae2b5f1de
Sabare1 <sabaregirivasan@gmail.com>
2025-08-12 12:54:07 +0530

    done with the setup for using AI API's in chef claude

---
+++ chef-claude/src/components/IngredientsList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientsList(props){
+    const newIng = props.ingredients.map((item) => {
+                    return <li>{item}</li>
+    });
+    return(
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul>
+                {newIng}
+            </ul>
+            {props.ingredients.length > 3 && 
+                <div className='recipe-container'>
+                    <div className='about-recipe-gen'>
+                        <p id='hype-user'>Ready for a recipe?</p>
+                        <p id='actual-ins'>Generate a recipe from your set of ingredients</p>
+                    </div>
+                    <button className='get-recipe-btn' onClick={props.getRecipe}>Get a recipe</button>
+                </div>
+            }
+        </section>
+    )
+}
