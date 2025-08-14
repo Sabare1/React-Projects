e21f8a7a444e27b70f9a9ae7ab6384c9e08adc35
Sabare1 <sabaregirivasan@gmail.com>
2025-08-14 12:23:30 +0530

    completed the static page of meme generator

--- meme-generator/src/components/Header.jsx
+++ meme-generator/src/components/Header.jsx
@@ -1,9 +1,10 @@
+import "../App.css"
 import trollFace from "../assets/troll-face.png"
 
 export default function Header(){
     return(
         <header>
-            <img src={trollFace} alt={"troll face icon"}></img>
+            <img src={trollFace} alt="troll face icon" className="meme-gen-icon"></img>
             <h1>Meme Generator</h1>
         </header>
     )
--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,5 +1,30 @@
+import '../App.css'
+import memeImg from '../assets/one-does-not-simply-meme.jpg'
 export default function Main(){
     return(
-        <h2>I'm the body</h2>
+        <main className='main-container'>
+            <div className='form-container'>
+                <label htmlFor='top-text'>
+                    Top text
+                    <input 
+                        id='top-text'
+                        placeholder='one does not simply'
+                    ></input>
+                </label>
+                <label htmlFor='bottom-text'>
+                    Bottom text
+                    <input 
+                        id='bottom-text'
+                        placeholder='walk into mordor'
+                    ></input>
+                </label>
+                <button id='get-meme-btn'>Get a new meme image</button>
+            </div>
+            <div className='meme'>
+                <img src='http://i.imgflip.com/1bij.jpg' alt='generated-meme'></img>
+                <span className='top'>one does not simply</span>
+                <span className='bottom'>walk into mordor</span>
+            </div>
+        </main>
     )
 }
