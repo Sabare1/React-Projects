6db6ccce8b601086d3f6931822f0e876a0e9198f
Sabare1 <sabaregirivasan@gmail.com>
2025-08-17 10:39:05 +0530

    learnt about the cleanup functions of useeffect

--- practice/src/WindowTracker.jsx
+++ practice/src/WindowTracker.jsx
@@ -1,7 +1,21 @@
 import React from "react"
 
 export default function WindowTracker() {
+
+    const[size, setSize] = React.useState(window.innerWidth)
+
+    React.useEffect(() => {
+        function setWindowSize(){
+            console.log("resized")
+            setSize(window.innerWidth)
+        }
+        window.addEventListener("resize",setWindowSize)
+
+        return () => window.removeEventListener("resize", setWindowSize)
+
+    }, [])
+
     return (
-        <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {size}</h1>
     )
 }
